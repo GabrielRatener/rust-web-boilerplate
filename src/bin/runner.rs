40600436831d87e033aee2e31fbe8270f66deb1f
Gabriel Ratener <gabe9693@gmail.com>
2020-01-28 09:19:29 -0800

    Serve static files

--- src/bin/runner.rs
+++ src/bin/runner.rs
@@ -1,11 +1,17 @@
 use dotenv::dotenv;
 use std::env;
+use rocket_contrib::serve::StaticFiles;
 
 fn main() -> Result<(), String> {
     dotenv().ok();
 
     let config_name = env::var("CONFIG_ENV").expect("CONFIG must be set");
     let rocket = rust_web_boilerplate::rocket_factory(&config_name)?;
-    rocket.launch();
+    let static_dir = concat!(env!("CARGO_MANIFEST_DIR"), "/public");
+
+    rocket
+        .mount("/", StaticFiles::from(static_dir))
+        .launch();
+
     Ok(())
 }
